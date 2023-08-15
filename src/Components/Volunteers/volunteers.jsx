fbfa8f21dc72786e964635f88b118235eb76250d
prathamesh-1701 <pdantkale22@gmail.com>
2023-08-15 10:37:07 +0530

    Third commit

--- src/Components/Volunteers/volunteers.jsx
+++ src/Components/Volunteers/volunteers.jsx
@@ -7,9 +7,14 @@ import {
   Text,
   UnorderedList,
   ListItem,
-  Link,Flex,Button,Image, Avatar
+  Link,
+  Flex,
+  Button,
+  Image,
+  Avatar,
 } from '@chakra-ui/react';
-import Yashraj from './imgs/YashrajThikane.jpg'
+import Yashraj from './imgs/YashrajThikane.jpg';
+import Neha from './imgs/NehaKarpe.jpg';
 
 const VolunteerPage = () => {
   return (
@@ -49,114 +54,132 @@ const VolunteerPage = () => {
           </Link>{' '}
           today!
         </Text>
-
-        
       </Container>
-      
-      <Box >
-      
-      
-      
-      <Box width={'80%'} border={'1px'}  margin={'8%'} _hover={{border:"1px solid #4CAF50 "}} >
-               <Center> <Image
-               padding={'1%'}
-        borderRadius='full'
-        boxSize='150px'
-        src='https://bit.ly/dan-abramov'
-        alt='Dan Abramov'
-        />
-        <Text margin={'1%'}>Rescuing a dog was a life-changing experience that filled my heart with joy and purpose. The moment I laid eyes on my furry companion, I knew that my life was about to be enriched beyond measure.
 
-        Bringing this precious soul into my home was a journey of patience, compassion, and unwavering commitment. The transformation I witnessed in my rescued dog was nothing short of miraculous. From the timid and uncertain creature that first entered my life, emerged a confident, loving, and loyal friend.
+      <Flex direction="column" alignItems="center" marginBottom="4rem">
+        <Box
+          width={{ base: '100%', md: '80%' }}
+          border="1px"
+          margin={['8%', '8%', '2rem']}
+          _hover={{ border: '1px solid #4CAF50' }}
+        >
+          <Center>
+            <Image
+              padding="1%"
+              borderRadius="full"
+              boxSize="150px"
+              src="https://bit.ly/dan-abramov"
+              alt="Dan Abramov"
+            />
+            <Text margin="1%">
+            Rescuing a dog was a life-changing experience that filled my heart with joy and purpose. The moment I laid eyes on my furry companion, I knew that my life was about to be enriched beyond measure.
 
-        Every day felt like a new adventure as we navigated life together. The bond we developed was forged through shared moments of triumph and growth. Teaching my rescued dog to trust, to love, and to embrace the world around us was a privilege that I will forever cherish.
+Bringing this precious soul into my home was a journey of patience, compassion, and unwavering commitment. The transformation I witnessed in my rescued dog was nothing short of miraculous. From the timid and uncertain creature that first entered my life, emerged a confident, loving, and loyal friend.
 
-        This experience taught me the true meaning of unconditional love. The joy in those wagging tails, the warmth of those furry cuddles, and the unwavering companionship during both sunny days and stormy nights are priceless treasures that have profoundly impacted my life.
+Every day felt like a new adventure as we navigated life together. The bond we developed was forged through shared moments of triumph and growth. Teaching my rescued dog to trust, to love, and to embrace the world around us was a privilege that I will forever cherish.
 
-        </Text>
-
-        </Center>
-        <Box 
-        
-        backgroundColor={'Trasperant'} width={'100%'} display={'flex'} justifyContent={'flex-end'} ><Text marginRight={'8%'} as='b'> - Mike Ross </Text></Box>
+This experience taught me the true meaning of unconditional love. The joy in those wagging tails, the warmth of those furry cuddles, and the unwavering companionship during both sunny days and stormy nights are priceless treasures that have profoundly impacted my life.
+              {/* Rest of the text */}
+            </Text>
+          </Center>
+          <Box backgroundColor="transparent" width="100%" display="flex" justifyContent="flex-end">
+            <Text marginRight="8%" as="b">
+              - Mike Ross{' '}
+            </Text>
+          </Box>
         </Box>
-        <Box width={'80%'} border={'1px'} margin={'8%'} _hover={{border:"1px solid #4CAF50 "}} >
-               <Center> <Image
-               padding={'1%'}
-        borderRadius='full'
-        boxSize='150px'
-        src={Yashraj}
-        alt='Dan Abramov'
-        />
-        <Text margin={'1%'}>Rescuing a dog was a life-changing experience that filled my heart with joy and purpose. The moment I laid eyes on my furry companion, I knew that my life was about to be enriched beyond measure.
+        <Box
+          width={{ base: '100%', md: '80%' }}
+          border="1px"
+          margin={['8%', '8%', '2rem']}
+          _hover={{ border: '1px solid #4CAF50' }}
+        >
+          <Center>
+            <Image
+              padding="1%"
+              borderRadius="full"
+              boxSize="150px"
+              src={Yashraj}
+              alt="Dan Abramov"
+            />
+            <Text margin="1%">
+            Rescuing a dog was a life-changing experience that filled my heart with joy and purpose. The moment I laid eyes on my furry companion, I knew that my life was about to be enriched beyond measure.
 
-        Bringing this precious soul into my home was a journey of patience, compassion, and unwavering commitment. The transformation I witnessed in my rescued dog was nothing short of miraculous. From the timid and uncertain creature that first entered my life, emerged a confident, loving, and loyal friend.
+Bringing this precious soul into my home was a journey of patience, compassion, and unwavering commitment. The transformation I witnessed in my rescued dog was nothing short of miraculous. From the timid and uncertain creature that first entered my life, emerged a confident, loving, and loyal friend.
 
-        Every day felt like a new adventure as we navigated life together. The bond we developed was forged through shared moments of triumph and growth. Teaching my rescued dog to trust, to love, and to embrace the world around us was a privilege that I will forever cherish.
+Every day felt like a new adventure as we navigated life together. The bond we developed was forged through shared moments of triumph and growth. Teaching my rescued dog to trust, to love, and to embrace the world around us was a privilege that I will forever cherish.
 
-        This experience taught me the true meaning of unconditional love. The joy in those wagging tails, the warmth of those furry cuddles, and the unwavering companionship during both sunny days and stormy nights are priceless treasures that have profoundly impacted my life.
-
-        </Text>
-
-        </Center>
-        <Box 
-        
-        backgroundColor={'Trasperant'} width={'100%'} display={'flex'} justifyContent={'flex-end'} ><Text marginRight={'8%'} as='b'> - Yashraj Thikane </Text></Box>
+This experience taught me the true meaning of unconditional love. The joy in those wagging tails, the warmth of those furry cuddles, and the unwavering companionship during both sunny days and stormy nights are priceless treasures that have profoundly impacted my life.
+              {/* Rest of the text */}
+            </Text>
+          </Center>
+          <Box backgroundColor="transparent" width="100%" display="flex" justifyContent="flex-end">
+            <Text marginRight="8%" as="b">
+              - Yashraj Thikane{' '}
+            </Text>
+          </Box>
         </Box>
-        <Box width={'80%'} border={'1px'} margin={'8%'} _hover={{border:"1px solid #4CAF50 "}} >
-               <Center> 
-               {/* <Image
-               padding={'1%'}
-        borderRadius='full'
-        boxSize='150px'
-        src='https://bit.ly/dan-abramov'
-        alt='Dan Abramov'
-        /> */}
-        <Avatar 
-        padding={'1%'}
-        borderRadius='full'
-        boxSize='150px'
-        // src='https://bit.ly/dan-abramov'
-        alt='Dan Abramov'></Avatar>
-        <Text margin={'1%'}>I adopted a dog from an adoption page. It was difficult to convince my parents as I had lost my dog a month before (due to death). 
-        Anyway, I convinced them. While adopting the dog, the aunt who was handling the dogs had tears in her eyes. She was thankful for us. She couldn't be happier. 
-        It was a little difficult for us to take away the dog from her sisters and mother, but we thought he would have a better life with us rather than on the streets. 
-        It was the best decision for him and us. Our home was filled with cuteness. Everyone was well taken care of. It was a bit challenging at first to make him behave well,
-         but with time and good parenting, we made him a well-behaved dog. We now live happily. I believe Dog adoption is important because it changes their lives and ours. 
-         It brings us happiness and makes us believe in ourselves and our patience as parents. It's not about the breed, but how we perceive it and instill values. 
-          Additionally adopting a street dog promotes compassion and ki
-        </Text>
+        <Box
+          width={{ base: '100%', md: '80%' }}
+          border="1px"
+          margin={['8%', '8%', '2rem']}
+          _hover={{ border: '1px solid #4CAF50' }}
+        >
+          <Center>
+            <Image
+              padding="1%"
+              borderRadius="full"
+              boxSize="150px"
+              src={Neha}
+              alt="Dan Abramov"
+            />
+            <Text margin="1%">
+            Rescuing a dog was a life-changing experience that filled my heart with joy and purpose. The moment I laid eyes on my furry companion, I knew that my life was about to be enriched beyond measure.
 
-        
-        
+Bringing this precious soul into my home was a journey of patience, compassion, and unwavering commitment. The transformation I witnessed in my rescued dog was nothing short of miraculous. From the timid and uncertain creature that first entered my life, emerged a confident, loving, and loyal friend.
 
-        </Center>
-        
-        <Box 
-        
-        backgroundColor={'Trasperant'} width={'100%'} display={'flex'} justifyContent={'flex-end'} ><Text marginRight={'8%'} as='b'> - Neha Karpe </Text></Box>
-        </Box>  
+Every day felt like a new adventure as we navigated life together. The bond we developed was forged through shared moments of triumph and growth. Teaching my rescued dog to trust, to love, and to embrace the world around us was a privilege that I will forever cherish.
+
+This experience taught me the true meaning of unconditional love. The joy in those wagging tails, the warmth of those furry cuddles, and the unwavering companionship during both sunny days and stormy nights are priceless treasures that have profoundly impacted my life.
+              {/* Rest of the text */}
+            </Text>
+          </Center>
+          <Box backgroundColor="transparent" width="100%" display="flex" justifyContent="flex-end">
+            <Text marginRight="8%" as="b">
+              - Neha Karpe{' '}
+            </Text>
+          </Box>
         </Box>
-        <Center><Text bgGradient='linear(to-l, #58b75c, #c6e6c7)'
-        bgClip='text' fontSize={'4xl'}>Thanks for readining</Text></Center>
+        {/* Other testimonial boxes */}
+      </Flex>
 
       <Center>
-      <Button
-              marginY="1rem"
-              color="#ffffff"
-              backgroundColor="#4CAF50"
-              className="FormFill"
-              _hover={{
-                bg: '#5cb860',
-                cursor: 'pointer',
-              }}
-            >
-              Show more
-            </Button>
-            </Center>
-      
-       {/* Footer */}
-       <Box className="footer">
+        <Text
+          bgGradient="linear(to-l, #58b75c, #c6e6c7)"
+          bgClip="text"
+          fontSize="4xl"
+        >
+          Thanks for reading
+        </Text>
+      </Center>
+
+      <Center>
+        <Button
+          marginY="1rem"
+          color="#ffffff"
+          backgroundColor="#4CAF50"
+          className="FormFill"
+          _hover={{
+            bg: '#5cb860',
+            cursor: 'pointer',
+          }}
+        >
+          Show more
+        </Button>
+      </Center>
+
+      {/* Footer */}
+      <Box className="footer">
         <Box className="footer-content">
           <Text>&copy; 2023 PetPal Center. All rights reserved.</Text>
           <Text>Prathamesh Dantkale</Text>
@@ -173,10 +196,7 @@ const VolunteerPage = () => {
           </Flex>
         </Box>
       </Box>
-      
-
     </Box>
-    
   );
 };
 
